b2fa9f32ae28e3b240a87f61bc98427c1a190613
PradyunS <pradyun.setti@gmail.com>
2020-05-09 17:33:24 +1200

    5m09a -initial commit: add files

---
+++ src/app/App.java
@@ -0,0 +1,15 @@
+package app;
+
+public class App {
+    public static void main(String[] args)  {
+        String selection;
+        boolean valid;
+
+        //display title sequence
+        System.out.println("#########################################################");
+        System.out.println("##########  Dwight Schrute's Gym For Muscules  ##########");
+        System.out.println("#################  by Pradyun Setti  ####################");
+        System.out.println("#########################################################");
+
+    }
+}
